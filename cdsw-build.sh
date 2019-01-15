421d56a008dc86fb296c56ebfa771c5001bef729
Test Admin <test-admin@sense.cloudera.com>
2019-01-15 12:28:27 +0000

    fix build

--- cdsw-build.sh
+++ cdsw-build.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-
-if [ -d "models/spark/" ] 
-then
-  rm -r -f models/spark/*
-else
-  mkdir -p models/spark
-fi
-
-if [ -f "spark_rf.tar" ]
-then 
-  tar -xf spark_rf.tar 
-fi
-
-if [ -f "sklearn_rf.pkl" ]
-then 
-  mv sklearn_rf.pkl models
-fi
+pip3 install sklearn
